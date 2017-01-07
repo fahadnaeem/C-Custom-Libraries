0c3980d9c19313927f9a78c17dfe42cb66309d15
fahad <fahad@123.com>
2017-01-07 11:59:32 -0800

    Functionality description added to fast_strings.h file

--- fast_strings.c
+++ fast_strings.c
@@ -1,12 +1,4 @@
 #include<stdio.h>
-/* 
- *  Author : Fahad Naeem
- *  Date : Jan 2, 2017
- *  Processor friendly function which gets the len of the string
- * Parameters:
- *  Input : C String
- *  Return: Integer value contains the length of C String
- */
 int get_len(char *str)
 {
 	int str_len=0;
--- fast_strings.h
+++ fast_strings.h
@@ -1,1 +1,11 @@
+/* 
+ *  Author : Fahad Naeem
+ *  Date : Jan 2, 2017
+ *  Processor friendly function which gets the len of the string
+ * Parameters:
+ *  Input : C String
+ *  Return: Integer value contains the length of C String
+ */
+
+
 int get_len(char *);
